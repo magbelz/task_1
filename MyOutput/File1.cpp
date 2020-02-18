0f813faf7bb13e72465a1e9b27870624c9da2eaf
Margarita <margaritabelousova4@gmail.com>
2020-02-19 03:48:48 +0600

    Добавлены выводы. Исправлена ошибка предыдущего коммита в котором не были сохранены изменения.

--- MyOutput/File1.cpp
+++ MyOutput/File1.cpp
@@ -1,16 +1,45 @@
 #pragma hdrstop
 #pragma argsused
-
-#ifdef _WIN32
 #include <tchar.h>
+#ifdef _WIN32
+#include <iostream>
 #else
   typedef char _TCHAR;
   #define _tmain main
 #endif
-
+#include <windows.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include "../MyDLL/Project1PCH1.h"
 
- int _tmain(int argc, _TCHAR* argv[]) 
+using namespace std;
+
+ int _tmain(int argc, _TCHAR* argv[])
 {
+	HINSTANCE MyDLL;
+	typedef int(__stdcall*pfgetDisk)(FAT32_BootRecord* pBootRecord);
+	pfgetDisk getDisk;
+
+	if ((MyDLL = LoadLibrary("Project1")) == NULL) {
+		 cout << "Cannot load DLL" << endl;
+		 cin.get();
+		 return 0;
+	}
+
+	if ((getDisk = (pfgetDisk)GetProcAddress(MyDLL, "getDisk")) == NULL) {
+		 cout << "Cannot find DLL function" << endl;
+		 cin.get();
+		 return 0;
+	}
+
+
+	FAT32_BootRecord boot_rec;
+	getDisk(&boot_rec);
+	cout << "OEM Name: " << boot_rec.BS_OEMName << endl;
+	cout << "Bytes per sector: " << boot_rec.BS_BytsPerSec << endl;
+	cout << "Reserved sectors count: " << boot_rec.BS_RsvdSecCnt << endl;
+	cin.get();
+
+	FreeLibrary(MyDLL);
 	return 0;
 }
--- MyOutput/Project2PCH1.h
+++ MyOutput/Project2PCH1.h
@@ -2,3 +2,4 @@
 #include <tchar.h>
 #endif
 
+
